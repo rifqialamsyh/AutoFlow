5fc644e1b904e3f7aaf46835d5d0b18482863e34
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2024-03-16 15:04:36 +0700

    add some function to detect and send data

--- AutoFlowWaterInfo/src/main.cpp
+++ AutoFlowWaterInfo/src/main.cpp
@@ -1,24 +1,43 @@
 #include <SPI.h>
+#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+const int trigPin = 2; // Ultrasonic sensor trigger pin
+const int echoPin = 4; // Ultrasonic sensor echo pin
 
-const byte address[6] = "00001";
+RF24 radio(7, 8); // CE, CSN pins
+
+struct UltrasonicData {
+  float distance;
+};
 
 void setup() {
   Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening(); // Set as transmitter
+  radio.openWritingPipe(0xF0F0F0F0E2LL); // Set a different address for communication
+  radio.setPALevel(RF24_PA_LOW);        // Set power level
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  
-  Serial.println("Sending message: ");
-  Serial.println(text);
+  // Ultrasonic sensor operations
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.034 / 2; // Calculate distance in cm
 
-  delay(1000);
+  // Bundle the ultrasonic data
+  UltrasonicData ultrasonicData;
+  ultrasonicData.distance = distance;
+
+  // Send the data via NRF24L01
+  radio.write(&ultrasonicData, sizeof(ultrasonicData));
+  Serial.print("Distance:");
+  Serial.println(ultrasonicData.distance);
+  // Wait before sending again
+  delay(1000); // Delay for 2 seconds
 }
