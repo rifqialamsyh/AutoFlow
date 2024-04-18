b3648c41770819385ba63c66ae5462230b8e61bd
Rifqi Alamsyah <rifqialamsyh@gmail.com>
2024-04-18 22:09:22 +0700

    sort get respone by date

--- AutoFlowAPI-Py/handler.py
+++ AutoFlowAPI-Py/handler.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 mysql = MySQL()
 # Konfigurasi koneksi MySQL
-app.config['MYSQL_DATABASE_USER'] = 'yourUSER'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'yourPASSWORD'
-app.config['MYSQL_DATABASE_DB'] = 'yourDATABASE'
-app.config['MYSQL_DATABASE_HOST'] = 'yourHOST'
+app.config['MYSQL_DATABASE_USER'] = 'user'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'database'
+app.config['MYSQL_DATABASE_HOST'] = 'host'
 
 mysql.init_app(app)
 
@@ -19,7 +19,7 @@ def showData():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    query = "SELECT * FROM data"
+    query = "SELECT * FROM data ORDER BY date DESC"
     
     try:
         cursor.execute(query)
